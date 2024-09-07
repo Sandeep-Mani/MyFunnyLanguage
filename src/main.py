59f04178d350ba95fc0d1b953dbbb10da4757cec
Sandeep Mani Kumar Kushwaha <sandeepmani2312@gmail.com>
2024-09-07 15:26:34 +0530

    Added run.sh and Adding ast

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 # https://medium.com/@marcelogdeandrade/writing-your-own-programming-language-and-compiler-with-python-a468970ae6df
 from lexer import Lexer
+from pathlib import Path
+
+
 def main():
     print("Hello world")
-    inputText = """
-        print(1 + (1 - 1*2))
-    """
+    inputText = Path('src/testProgram.mfl').read_text();
     lexer = Lexer().get_lexer()
     for token in lexer.lex(inputText):
         print(token)
